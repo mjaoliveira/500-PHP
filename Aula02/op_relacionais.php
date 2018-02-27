53a0b0d16629b30abce17f323293c3119d6d8995
Marcelo Jorge <marcelojorge.ba@gmail.com>
2018-02-27 20:29:14 -0300

    op relacionais

---
+++ Aula02/op_relacionais.php
@@ -0,0 +1,35 @@
+<?php
+
+var_dump(2 == 2);
+var_dump(2 == 3);
+var_dump(2 == "2");
+echo "<hr>";
+
+var_dump(2 === 2);
+var_dump(2 === "2");
+echo "<hr>";
+
+var_dump(2 != 2);
+var_dump(2 != 3);
+echo "<hr>";
+
+var_dump(2 < 5);
+var_dump(2 < 2);
+var_dump(2 < 1);
+echo "<hr>";
+
+var_dump(2 > 5);
+var_dump(2 > 2);
+var_dump(2 > 1);
+echo "<hr>";
+
+var_dump(2 <= 5);
+var_dump(2 <= 2);
+var_dump(2 <= 1);
+echo "<hr>";
+
+var_dump(2 >= 5);
+var_dump(2 >= 2);
+var_dump(2 >= 1);
+echo "<hr>";
+  ?>
