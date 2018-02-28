0c82743a27cdd93b2202a2df95b7570d09f392c0
Marcelo Jorge <marcelojorge.ba@gmail.com>
2018-02-27 22:15:16 -0300

    atualizando22:15

---
+++ Aula02/switch.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$nome = readline("Digite o seu nome:");
+
+
+switch ($nome) {
+	case 'Marcelo':
+		echo "E aew Marcelo";
+		break;
+	case 'Marcos':
+		echo "E aew Marcos";
+		break;
+	case 'Maria':
+		echo "E aew Maria";
+		break;
+	
+	default:
+		echo "E ae Estranho";
+		break;
+}
+
+?>
