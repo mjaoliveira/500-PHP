c1035adf6f50c84da0329ca901338bdce6404acd
Marcelo Jorge <marcelojorge.ba@gmail.com>
2018-02-28 20:05:57 -0300

    Aula03 20:05

---
+++ Aula03/do_while.php
@@ -0,0 +1,13 @@
+<?php
+
+$cont = 0;
+
+
+do{
+	echo "$cont...";
+	$cont ++;
+}while ($cont <= 10) ;
+
+echo "Acabou...";
+
+?>
---
+++ Aula03/while.php
@@ -0,0 +1,11 @@
+<?php
+
+$cont = 1;
+
+while ($cont <= 10) {
+	echo "$cont...<br/>";
+	$cont++;
+}
+
+echo "Acabou...";
+?>
